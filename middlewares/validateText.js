4bdfba033966e5d1d3e4dc724fd3138c0e495bde
Marcelo Romitti <romittimarcelo@gmail.com>
2021-06-06 17:37:01 -0300

    add middlewares

---
+++ middlewares/validateText.js
@@ -0,0 +1,18 @@
+
+const validateText = (req, res, next) => {
+
+    const textRequired = req.query.text;
+
+    if ( textRequired === '' ) {
+        return res.status(400).json({
+           text: "no text"
+        });
+    }
+
+    next();
+}
+
+module.exports = {
+    validateText
+}
+
--- routes/texts.js
+++ routes/texts.js
@@ -3,12 +3,12 @@
     /iecho?text=
 */
 const { Router } = require('express');
-
+const { validateText } = require('../middlewares/validateText')
 const { getText } = require('../controllers/texts');
 
 const router = Router();
 
 // Get Text 
-router.get('/', getText );
+router.get('/', validateText, getText );
 
 module.exports = router;
